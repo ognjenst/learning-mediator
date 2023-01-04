c2ceef7ab631e7a2f92e8a926341121154c3f855
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2023-01-04 21:40:04 +0100

    Even more elegant way

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -9,10 +9,8 @@ class Program
     private static void Main(string[] args)
     {
         var mediator = new ConcreteMediator();
-        var c1 = new Colleague1();
-        var c2 = new Colleague2();
-        mediator.Register(c1);
-        mediator.Register(c2);
+        var c1 = mediator.CreateColleague<Colleague1>();
+        var c2 = mediator.CreateColleague<Colleague2>();
         
         c1.Send("How are you?");
         c2.Send("Fine, thanks");
--- Introduction/Structural/ConcreteMediator.cs
+++ Introduction/Structural/ConcreteMediator.cs
@@ -10,6 +10,14 @@ public class ConcreteMediator : Mediator
         colleague.SetMediator(this);
         _colleagues.Add(colleague);
     }
+
+    public T CreateColleague<T>() where T : Colleague, new()
+    {
+        var colleague = new T();
+        colleague.SetMediator(this);
+        _colleagues.Add(colleague);
+        return colleague;
+    }
     public override void Send(string message, Colleague colleague)
     {
         _colleagues.Where(c => c != colleague).ToList().ForEach(c => c.HandleNotification(message));
